7bcd6bc95eb6d0625219a41d7b97d10c16ca8c2a
oswizar <oswizar@icloud.com>
2023-06-09 20:24:01 +0800

    correct test.

--- src/test/java/com/oswizar/springbootsample/SpringBootApplicationTest.java
+++ src/test/java/com/oswizar/springbootsample/SpringBootApplicationTest.java
@@ -1,34 +1,29 @@
 package com.oswizar.springbootsample;
 
 import com.oswizar.springbootsample.controller.HelloWorldController;
-import com.oswizar.springbootsample.entity.User;
-import com.oswizar.springbootsample.util.*;
+import com.oswizar.springbootsample.util.HttpUtils;
+import com.oswizar.springbootsample.util.Md5Utils;
 import lombok.extern.slf4j.Slf4j;
 import org.junit.Before;
 import org.junit.Test;
-import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.http.MediaType;
-import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 import org.springframework.test.web.servlet.result.MockMvcResultHandlers;
 import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 
 import javax.sql.DataSource;
-
 import java.sql.Connection;
 import java.sql.SQLException;
 import java.util.Arrays;
-import java.util.Date;
 import java.util.List;
 
 import static org.hamcrest.Matchers.equalTo;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
-@RunWith(SpringRunner.class)
 @SpringBootTest
 @Slf4j
 public class SpringBootApplicationTest {
@@ -38,10 +33,6 @@ public class SpringBootApplicationTest {
     @Autowired
     DataSource dataSource;
 
-    @Autowired
-    User user;
-
-
     @Before
     public void setUp() {
         mvc = MockMvcBuilders.standaloneSetup(new HelloWorldController()).build();
@@ -49,21 +40,17 @@ public class SpringBootApplicationTest {
 
     @Test
     public void helloWorld() throws Exception {
-        mvc.perform(MockMvcRequestBuilders.get("/hello").accept(MediaType.APPLICATION_JSON))
-                .andExpect(status().isOk())
-                .andDo(MockMvcResultHandlers.print())
-                .andExpect(content().string(equalTo("Hello World")));
+        mvc.perform(MockMvcRequestBuilders.get("/hello").accept(MediaType.APPLICATION_JSON)).andExpect(status().isOk()).andDo(MockMvcResultHandlers.print()).andExpect(content().string(equalTo("Hello World")));
 
-}
+    }
 
     @Test
     public void dataBundleTest() {
 
         String str = "dsdfsdfkjsfiej235";
-        String subStr = str.substring(1,6);
+        String subStr = str.substring(1, 6);
         System.out.println(subStr);
         System.out.println(str);
-        System.out.println(user);
     }
 
 
@@ -82,7 +69,6 @@ public class SpringBootApplicationTest {
     }
 
 
-
     @Test
     public void contextLoad() throws SQLException {
         Connection connection = dataSource.getConnection();
@@ -92,14 +78,11 @@ public class SpringBootApplicationTest {
     }
 
 
-
-
-
     // 编程技巧:使用equals判断的时候,要把已知的变量写在前边,未知的写在后边,防止空指针异常
     @Test
     public void equalsTest() {
         String index = null;
-        if("oooo".equals(index)) {
+        if ("oooo".equals(index)) {
             System.out.println("成功");
         } else {
             System.out.println("失败");
@@ -116,39 +99,6 @@ public class SpringBootApplicationTest {
 
 
     }
-
-    @Test
-    public void dateTest() {
-        Date date = DateUtils.getCurrentDate();
-        System.out.println(DateUtils.getCurrentFormatDate("yyyy-MM"));
-        System.out.println(DateUtils.getMonthStr(date));
-
-        System.out.println(DateUtils.getYear());
-
-        User user = new User();
-        user.setUserId(1);
-        user.setUserName("tom");
-        user.setPassword("tomcat");
-
-
-        String xml = XmlUtils.toXML(user);
-        System.out.println(xml);
-        System.out.println(XmlUtils.xmlToObject(xml, user.getClass()));
-
-    }
-
-    @Test
-    public void sftpTest() throws Exception {
-
-        SFTPUtils instance = SFTPUtils.getInstance("10.10.10.136", 2222, "abc", "abc", "", "");
-        System.out.println(instance);
-//        instance.uploadFile("/up", "D:\\", "redis.conf");
-//        instance.uploadFile("/upload/", "/example/", "D:\\","CACHE.log|redis.conf");
-
-//        System.out.println("helloworld".substring(2,5));
-        instance.downLoadFile("/upload/example/", "redis.conf", "d:\\sftp");
-    }
-
 
     @Test
     public void md5Test() {
